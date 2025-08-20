02225472b68f3535d2c1b24e68cf07d8a82cec66
lirongyaoper <lirongyaoper@163.com>
2025-08-20 21:58:28 +0800

    update data at 20250820215828

--- Ubuntu/bashrc/bashrc.sh
+++ Ubuntu/bashrc/bashrc.sh
@@ -154,6 +154,13 @@ alias gitlpy1='cd /home/lirongyao0916/Projects/Py1_project/ &&  git pull'
 alias gitcpy1='cd /home/lirongyao0916/Projects && rm -rf ./Py1_project && git clone  git@github.com:lirongyaoper/Py1_project.git'
 
 
+################    Py2_project    ##################
+alias githpy2='cd /home/lirongyao0916/Projects/Py2_project/ &&  git add . && git commit -m "update data at $(date +%Y%m%d%H%M%S)" && git push'
+alias gitlpy2='cd /home/lirongyao0916/Projects/Py2_project/ &&  git pull'
+alias gitcpy2='cd /home/lirongyao0916/Projects && rm -rf ./Py2_project &&  git clone git@github.com:lirongyaoper/Py2_project.git'
+##############    py2_project   end  ################
+
+
 ##########        Php1_project     ###################
 alias githphp1='cd /home/lirongyao0916/Projects/Php1_project/ &&  git add . && git commit -m "update data at $(date +%Y%m%d%H%M%S)" && git push'
 alias gitlphp1='cd /home/lirongyao0916/Projects/Php1_project/ &&  git pull'
@@ -172,17 +179,11 @@ alias gitlc1='cd /home/lirongyao0916/Projects/C1_Project/ &&  git pull'
 alias gitcc1='cd /home/lirongyao0916/Projects && rm -rf ./C1_Project &&  git clone git@github.com:lirongyaoper/C1_Project.git'
 
 
-################    Py2_project    ##################
-alias githpy2='cd /home/lirongyao0916/Projects/Py2_project/ &&  git add . && git commit -m "update data at $(date +%Y%m%d%H%M%S)" && git push'
-alias gitlpy2='cd /home/lirongyao0916/Projects/Py2_project/ &&  git pull'
-alias gitcpy2='cd /home/lirongyao0916/Projects && rm -rf ./Py2_project &&  git clone git@github.com:lirongyaoper/Py2_project.git'
-##############    py2_project   end  ################
-
 
 #############      lryper   start   #################
-alias githlry='cd /home/lirongyao0916/Projects/lryper.com/ &&  git add . && git commit -m "update data at $(date +%Y%m%d%H%M%S)" && git push'
-alias gitllry='cd /home/lirongyao0916/Projects/lryper.com/ &&  git pull'
-alias gitclry='cd /home/lirongyao0916/Projects/ && sudo rm -rf ./lryper.com &&  git clone git@github.com:lirongyaoper/lryper.com.git'
+alias githper='cd /home/lirongyao0916/Projects/lryper.com/ &&  git add . && git commit -m "update data at $(date +%Y%m%d%H%M%S)" && git push'
+alias gitlper='cd /home/lirongyao0916/Projects/lryper.com/ &&  git pull'
+alias gitcper='cd /home/lirongyao0916/Projects/ && sudo rm -rf ./lryper.com &&  git clone git@github.com:lirongyaoper/lryper.com.git'
 
 #############     lryper  end       ################
 
